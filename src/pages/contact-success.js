882d9fbd9f550f45d5ae942b2a0a6e0a1359f574
Jakedalus <jacquedalus@gmail.com>
2020-04-21 15:32:37 -0400

    Add basic styles to contact-success

--- src/pages/contact-success.js
+++ src/pages/contact-success.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import Layout from '../components/layout';
+import styled from 'styled-components';
+
+const ContentWrapper = styled.section`
+  max-width: 800px;
+  margin: 40px auto;
+  background: var(--page_background);
+  color: var(--page_color);
+  padding: 20px;
+  border-radius: 10px;
+
+  h1 {
+    color: var(--page_heading);
+  }
+
+  a {
+    color: var(--yellow);
+  }
+`;
 
 const ContactSuccess = () => {
   return (
     <Layout>
-      <h1>Thank you for getting in touch!</h1>
+      <ContentWrapper>
+        <h1>Thank you for getting in touch!</h1>
+      </ContentWrapper>
     </Layout>
   )
 };
